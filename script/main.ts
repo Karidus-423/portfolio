5062c286c0d80c762d1a4b2f8d8f765a93720394
Karidus-423 <kapuerto23@gmail.com>
2025-05-08 00:34:08 +0000

    github pagees

--- script/main.ts
+++
@@ -1,4 +0,0 @@
-import idk from "./navbar.ts";
-const parent_nav = document.body.querySelector(".navbar");
-
-idk(parent_nav);
--- script/navbar.ts
+++
@@ -1,8 +0,0 @@
-function idk(parent_nav?: Element) {
-  if (parent_nav != null) {
-    const headingText = document.createTextNode("Big Head!");
-    parent_nav.appendChild(headingText);
-  }
-}
-
-export default idk;
